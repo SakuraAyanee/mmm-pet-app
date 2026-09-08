0140a13f80bc9684d338aac081641555bc5e274f
SakuraAyanee <15012170236@163.com>
2026-09-08 09:38:13 +0800

    初始化宠物样式，搭建第一块积木PetAvatar，创建第一个section填充

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+import PetAvatar from './components/PetAvatar.vue'
+</script>
+
 <template>
-  <main class="pet-app"></main>
+  <main class="pet-app">
+    <PetAvatar />
+  </main>
 </template>
+
+<style scoped>
+.pet-app {
+  display: grid;
+  min-height: 100vh;
+  place-items: center;
+}
+</style>
